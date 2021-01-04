b197ef00bbdf36062ea9e993b4b7dd9a3ddcba49
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-04 18:03:27 +0900

    fix tsconfig include path

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,6 @@ module.exports = {
             loader: "ts-loader",
           },
         ],
-        // include: [
-        //   path.resolve(__dirname, "src"),
-        //   path.resolve(__dirname, "node_modules/goober"),
-        // ],
-        exclude: /node_modules/,
       },
     ],
   },
